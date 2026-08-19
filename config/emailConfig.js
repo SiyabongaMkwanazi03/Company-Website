899801c0ec2a9304dccdf7174560d220379621f2
SiyabongaMkwanazi03 <siyabongamkwanazi03@gmail.com>
2026-08-20 00:01:14 +0200

    Use SMTP port 587 for production

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,15 +2,17 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtpout.secureserver.net",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
+    requireTLS: true,
+
     auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD,
     },
 });
 
-console.log("SMTP HOST: smtpout.secureserver.net");
+console.log("SMTP HOST:", "smtpout.secureserver.net");
 console.log("SMTP USER:", process.env.SMTP_EMAIL);
 
 transporter.verify((error, success) => {
